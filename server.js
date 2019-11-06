497c239000f08c69a2e81c304fc113612e3571b3
Mauricio503 <mauriciogspagnol@gmail.com>
2019-11-06 17:34:48 +0000

    pronto

--- server.js
+++ server.js
@@ -1,7 +1,23 @@
 // importa o express
 const express = require('express');
-// executa funcao
+//importa mongo, para instalar utlizar no terminal npm install mongoose
+const mongoose = require('mongoose');
+
+// iniciando do app
 const app  = express();
+
+// iniciando banco de dados
+const MongoClient = require('mongodb').MongoClient;
+const uri = "mongodb://mauricio:mm1010mauri@cluster0-qaada.mongodb.net/test?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true ,useUnifiedTopology: true});
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  client.close();
+});
+
+    //mongoose.connect('mongodb://mauricio:mm1010mauri@cluster0-qaada.mongodb.net/test?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
+
 
 // passando a rota, req(requesição), res (resposta)
 app.get('/', (req,res) => {
