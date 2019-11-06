4a6f14eeabf1b17d7ec6beb18f80f12f82ce28f7
Mauricio503 <mauriciogspagnol@gmail.com>
2019-11-06 13:55:51 +0000

    pronto

--- server.js
+++ server.js
@@ -2,5 +2,11 @@
 const express = require('express');
 // executa funcao
 const app  = express();
+
+// passando a rota, req(requesição), res (resposta)
+app.get('/', (req,res) => {
+    res.send("Hello");
+});
+
 // abre na porta 3001
 app.listen(3001);
