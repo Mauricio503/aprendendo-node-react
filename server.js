d6ecafc5a71b9d5c7414afee46fe08a04938b119
Mauricio503 <mauriciogspagnol@gmail.com>
2019-11-06 14:14:47 +0000

    pronto

--- server.js
+++ server.js
@@ -5,8 +5,14 @@ const app  = express();
 
 // passando a rota, req(requesição), res (resposta)
 app.get('/', (req,res) => {
-    res.send("Hello");
+    res.send("Hello World");
 });
+
+// ----------Nodemon----------
+// com o Nodemon é possivel atualizar as informações da pagina sem restartar
+// para instalar utlizar o codigo npm install -D nodemon
+// no arquivo package inserir nos scripts 'dev': 'nodemon server.js'
+// no terminal executar npm run dev
 
 // abre na porta 3001
 app.listen(3001);
