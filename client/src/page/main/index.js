80adc60d246be84eb99146fb8af8b6c249894ce5
Mauricio503 <mauriciogspagnol@gmail.com>
2019-11-14 19:14:47 +0000

    pronto

--- client/src/page/main/index.js
+++ client/src/page/main/index.js
@@ -2,6 +2,10 @@ import React, { Component } from 'react';
 import api from '../../service/api';
 
 export default class Main extends Component{
+    // state é um objeto
+    state = {
+        products: []
+    };
 
     //quando utilizando métodos do react o métodos são formandos com = () =>
     //caso contrário apenas ()
@@ -11,14 +15,21 @@ export default class Main extends Component{
         this.loadProducts();
     }
 
-    loadProducts = () => {
-        const response = api.get('/products');
-
-        console.log(response.data.docs);
+    loadProducts = async () => {
+        const response = await api.get('/products');
+        //popula objeto
+        this.setState({ products: response.data.docs });
     };
     
+    //percorre lista e mostra na tela
     render(){
-        return <h1>Hello</h1>
+        return (
+            <div className="product-list">
+                {this.state.products.map(product => (
+                    <h2 key={product._id}>{product.title}</h2>
+                ))}
+            </div>
+        )
     }
 
 
